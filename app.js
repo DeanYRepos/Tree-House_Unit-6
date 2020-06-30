f5dd006a256b574347fb62715e49f5be2d12a85e
DeansRepos <54600923+DeanYRepos@users.noreply.github.com>
2020-06-30 11:20:21 -0500

    changes to app.js to include live link

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const {projects} = require('./data.json');
-
+const port = process.env.PORT || 3000;
 const app = express();
 
 
@@ -9,20 +9,14 @@ app.set('view engine', 'pug');
 app.use(express.json());
 app.use('/static', express.static('public'));
 
-app.use((req, res, next) => {
-console.log('Oops!');
-
-const err = new Error ('Somthing went wrong!');
-err.status = 500;
-next(err);
-});
-
     
 app.get('/',(req, res, next) => {
     res.render('index', {projects});
+    next();
 }) 
 app.get('/about',(req,res,next) => {
     res.render('about');
+    next();
 })
 app.get('/projects/:id',(req,res,next) => {
     const projectId = req.params.id;
@@ -35,13 +29,21 @@ app.get('/projects/:id',(req,res,next) => {
         
         res.sendStatus(404);
     }
-    
+    next();
 });  
+
+app.use((req, res, next) => {
+
+    const err = new Error('Page Not Found');
+    err.status = 404;
+    next(err);
+})
 
 app.use((err, req, res, next) => {
     res.locals.error = err;
     res.status(err.status);
     res.render('error');
+    
   
 }); 
 
@@ -51,7 +53,7 @@ app.use((err, req, res, next) => {
 
 
 
-app.listen(3000, () => {
+app.listen(port, () => {
 
     console.log("Listening on port 3000!")
 });
